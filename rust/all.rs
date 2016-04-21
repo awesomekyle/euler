c4346d2e281975bb744dacb99800223073e009d7
Kyle Weicht <kyle@awesomekyle.com>
2016-04-20 23:53:42 -0700

    Thread results

--- rust/all.rs
+++ rust/all.rs
@@ -1,3 +1,6 @@
+use std::thread;
+use std::vec;
+
 mod e001;
 mod e002;
 mod e003;
@@ -5,9 +8,30 @@ mod e004;
 mod e005;
 
 fn main() {
-    println!("001: {}", e001::euler());
-    println!("002: {}", e002::euler());
-    println!("003: {}", e003::euler());
-    println!("004: {}", e004::euler());
-    println!("005: {}", e005::euler());
+    let functions: Vec<fn()->u64> = vec![
+         e001::euler,
+         e002::euler,
+         e003::euler,
+         e004::euler,
+         e005::euler,
+    ];
+    let mut threads = vec![];
+    let mut results: Vec<u64> = vec::Vec::new();
+    results.resize(functions.len(), 0);
+
+    // spawn threads for each project
+    for ii in 0..functions.len() {
+        let func = functions[ii];
+        threads.push(thread::spawn(move || {
+            return func();
+        }));
+    }
+
+    // print out all results
+    let mut ii = 0;
+    for thread in threads {
+        let result = thread.join();
+        println!("{}: {}", ii, result.unwrap());
+        ii += 1;
+    }
 }
