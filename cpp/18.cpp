8c798a91c257f3bf262d47ff61fd99cea07d3862
Kyle Weicht <kyle@awesomekyle.com>
2017-06-24 15:14:39 -0700

    WIP multiplication appears to work

--- cpp/18.cpp
+++ cpp/18.cpp
@@ -28,9 +28,9 @@ std::string add(std::string const& a, std::string const& b)
         if (index >= a.length() && index >= b.length() && carry == 0) {
             break;
         }
-        int x = get_column(a, index);
-        int y = get_column(b, index);
-        int sum = x + y + carry;
+        int const x = get_column(a, (a.length() - 1) - index);
+        int const y = get_column(b, (b.length() - 1) - index);
+        int const sum = x + y + carry;
         out << (sum % 10);
         carry = sum / 10;
         index++;
@@ -39,9 +39,31 @@ std::string add(std::string const& a, std::string const& b)
     std::reverse(ret.begin(), ret.end());
     return ret;
 }
-// std::string multiplication(std::string const& a, std::string const& b)
-//{
-//}
+
+std::string multiplication(std::string const& a, std::string const& b)
+{
+    // std::stringstream out;
+    std::string out;
+    int carry = 0;
+    int index = 0;
+    for (auto c0 = b.rbegin(); c0 != b.rend(); ++c0) {
+        int const x = dig(*c0);
+        std::string tmp;
+        int col = 1;
+        for (auto c1 = a.rbegin(); c1 != a.rend(); ++c1) {
+            int const y = dig(*c1);
+            tmp = add(tmp, std::to_string(y * x * col));
+            col *= 10;
+        }
+        int const trailing_zeros = c0 - b.rbegin();
+        tmp.insert(tmp.end(), trailing_zeros, '0');
+        out = add(out, tmp);
+        index++;
+    }
+    // auto ret = out.str();
+    // std::reverse(ret.begin(), ret.end());
+    return out;
+}
 
 // uint64_t factorial(uint64_t val)
 //{
@@ -63,7 +85,7 @@ int main()
 {
     //    uint64_t const fac = factorial(100);
     //    std::cout << digit_summer(fac) << "\n";
-    std::cout << add("1111111111111111111111111111111111111111111111",
-                     "333333333333333333333333333333333333333333333333333333333");
+    std::cout << multiplication("423", "211");
+    std::cout << add("12345", "123");
     return 0;
 }
